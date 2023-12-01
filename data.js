a7a449e83919732548a2d3d7a3851a32300bf6c7
Akash Srinivas <128985486+SulphuricVein@users.noreply.github.com>
2023-12-01 09:50:30 +0530

    Update data.js

--- data.js
+++ data.js
@@ -7,25 +7,25 @@ export const songData = {
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
     rock: [
       {
         title: "I Will Survie- Gloria Gaynor",
-        image: "/trackImages/iWillSurvive.png",
+        image: "trackImages/iWillSurvive.png",
       },
       {
         title: "Don't Stop Believin' - Journey",
-        image: "/trackImages/dontStopTheFeeling.png",
+        image: "trackImages/dontStopTheFeeling.png",
       },
       {
         title: "Here Comes the Sun - The Beatles",
@@ -33,61 +33,61 @@ export const songData = {
       },
       {
         title: "I'm a Believer - The Monkees",
-        image: "/trackImages/imBeliever.png",
+        image: "trackImages/imBeliever.png",
       },
     ],
     rap: [
       {
         title: "Wiggle-Jason Derulo feat. Snoop Dogg",
-        image: "/trackImages/wiggle.png",
+        image: "trackImages/wiggle.png",
       },
       {
         title: "Best Day Ever - Mac Miller",
-        image: "/trackImages/bestDayEver.png",
+        image: "trackImages/bestDayEver.png",
       },
       {
         title: "Good Life - Kanye West ft. T-Pain",
-        image: "/trackImages/goodLife.png",
+        image: "trackImages/goodLife.png",
       },
       {
         title: "I'm a Believer - The Monkees",
-        image: "/trackImages/imBeliever.png",
+        image: "trackImages/imBeliever.png",
       },
     ],
     jazz: [
       {
         title: "Take Five - Dave Brubeck",
-        image: "/trackImages/takeFive.png",
+        image: "trackImages/takeFive.png",
       },
       {
         title: "Best Day Ever - Mac Miller",
-        image: "/trackImages/bestDayEver.png",
+        image: "trackImages/bestDayEver.png",
       },
       {
         title: "Good Life - Kanye West ft. T-Pain",
-        image: "/trackImages/goodLife.png",
+        image: "trackImages/goodLife.png",
       },
       {
         title: "I'm a Believer - The Monkees",
-        image: "/trackImages/imBeliever.png",
+        image: "trackImages/imBeliever.png",
       },
     ],
     country: [
       {
         title: "Cotton Eye Joe- Rednex",
-        image: "/trackImages/cottonEyeJoe.png",
+        image: "trackImages/cottonEyeJoe.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
   },
@@ -95,91 +95,91 @@ export const songData = {
     pop: [
       {
         title: "Someone Like You - Adele",
-        image: "/trackImages/someoneLikeYou.png",
+        image: "trackImages/someoneLikeYou.png",
       },
       {
         title: "Best Day Ever - Mac Miller",
-        image: "/trackImages/bestDayEver.png",
+        image: "trackImages/bestDayEver.png",
       },
       {
         title: "Good Life - Kanye West ft. T-Pain",
-        image: "/trackImages/goodLife.png",
+        image: "trackImages/goodLife.png",
       },
       {
         title: "I'm a Believer - The Monkees",
-        image: "/trackImages/imBeliever.png",
+        image: "trackImages/imBeliever.png",
       },
     ],
     rock: [
       {
         title: "Hurt - Johnny Cash",
-        image: "/trackImages/hurt.png",
+        image: "trackImages/hurt.png",
       },
       {
         title: "I'm a Believer - The Monkees",
-        image: "/trackImages/imBeliever.png",
+        image: "trackImages/imBeliever.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
     rap: [
       {
         title: "Stan - Eminem",
-        image: "/trackImages/stan.png",
+        image: "trackImages/stan.png",
       },
       {
         title: "Best Day Ever - Mac Miller",
-        image: "/trackImages/bestDayEver.png",
+        image: "trackImages/bestDayEver.png",
       },
       {
         title: "Good Life - Kanye West ft. T-Pain",
-        image: "/trackImages/goodLife.png",
+        image: "trackImages/goodLife.png",
       },
     ],
     jazz: [
       {
         title: "Round Midnight - Miles Davis",
-        image: "/trackImages/roundMidnight.png",
+        image: "trackImages/roundMidnight.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
     country: [
       {
         title: "Blue Eyes Crying in the Rain - Willie Nelson",
-        image: "/trackImages/sadCountry.png",
+        image: "trackImages/sadCountry.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
   },
@@ -187,92 +187,92 @@ export const songData = {
     pop: [
       {
         title: "Can't Stop the Feeling! - Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
     rock: [
       {
         title: "Wish You Were Here - Pink Floyd",
-        image: "/trackImages/wishYouWereHere.png",
+        image: "trackImages/wishYouWereHere.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
     rap: [
       {
         title: "Changes - Tupac Shakur",
-        image: "/trackImages/changes.png",
+        image: "trackImages/changes.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
     jazz: [
       {
         title: "Stardust - The Cardigans",
-        image: "/trackImages/starDust.png",
+        image: "trackImages/starDust.png",
       },
 
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
     country: [
       {
         title: "Wagon Wheel - Darius Rucker",
-        image: "/trackImages/wagonWheel.png",
+        image: "trackImages/wagonWheel.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
   },
@@ -280,91 +280,91 @@ export const songData = {
     pop: [
       {
         title: "SHOTS - LMFAO",
-        image: "/trackImages/shots.png",
+        image: "trackImages/shots.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
     rock: [
       {
         title: "Tears Don't Fall - Bullet for my Valentine",
-        image: "/trackImages/tearsDontFall.png",
+        image: "trackImages/tearsDontFall.png",
       },
       {
         title: "Can't Stop the Feeling!- Justin Timberlake",
-        image: "/trackImages/cantStopTheFeeling.png",
+        image: "trackImages/cantStopTheFeeling.png",
       },
       {
         title: "'Walking on Sunshine-Katrina and the Waves",
-        image: "/trackImages/walkingFeeling.png",
+        image: "trackImages/walkingFeeling.png",
       },
       {
         title: "Good Vibrations - The Beach Boys",
-        image: "/trackImages/goodVibrations.png",
+        image: "trackImages/goodVibrations.png",
       },
     ],
     rap: [
       {
         title: "Lose Yourself - Eminem",
-        image: "/trackImages/loseYourself.png",
+        image: "trackImages/loseYourself.png",
       },
       {
         title: "Best Day Ever - Mac Miller",
-        image: "/trackImages/bestDayEver.png",
+        image: "trackImages/bestDayEver.png",
       },
       {
         title: "Good Life - Kanye West ft. T-Pain",
-        image: "/trackImages/goodLife.png",
+        image: "trackImages/goodLife.png",
       },
       {
         title: "I'm a Believer - The Monkees",
-        image: "/trackImages/imBeliever.png",
+        image: "trackImages/imBeliever.png",
       },
     ],
     jazz: [
       {
         title: "Pump Up the Jam - Technotronic",
-        image: "/trackImages/pumpUp.png",
+        image: "trackImages/pumpUp.png",
       },
       {
         title: "Best Day Ever - Mac Miller",
-        image: "/trackImages/bestDayEver.png",
+        image: "trackImages/bestDayEver.png",
       },
       {
         title: "Good Life - Kanye West ft. T-Pain",
-        image: "/trackImages/goodLife.png",
+        image: "trackImages/goodLife.png",
       },
       {
         title: "I'm a Believer - The Monkees",
-        image: "/trackImages/imBeliever.png",
+        image: "trackImages/imBeliever.png",
       },
     ],
     country: [
       {
         title: "Chicken Fried - Zac Brown Band",
-        image: "/trackImages/chickenFried.png",
+        image: "trackImages/chickenFried.png",
       },
       {
         title: "Best Day Ever - Mac Miller",
-        image: "/trackImages/bestDayEver.png",
+        image: "trackImages/bestDayEver.png",
       },
       {
         title: "Good Life - Kanye West ft. T-Pain",
-        image: "/trackImages/goodLife.png",
+        image: "trackImages/goodLife.png",
       },
       {
         title: "I'm a Believer - The Monkees",
-        image: "/trackImages/imBeliever.png",
+        image: "trackImages/imBeliever.png",
       },
     ],
   },
